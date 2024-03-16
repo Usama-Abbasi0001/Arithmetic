2aaf48afbfefa66e3df9e904734943143b756087
Usama-Abbasi0001 <157971981+Usama-Abbasi0001@users.noreply.github.com>
2024-03-16 16:35:23 +0500

    aritmetic

---
+++ second.js
@@ -0,0 +1,47 @@
+var myname ="this is var variables";
+document.write(myname);
+
+let mylet =" <br>this is let variables";
+document.write(mylet);
+
+let myconst =" <br>this is const variables";
+document.write(myconst);
+
+var x ="<br><br> this is String datatype uses in var variable <br> <br>";
+document.write( x);
+
+let z = 3409;
+document.write("This is Number datatype use in let variable <br> "+ z);
+
+const y = 'fales';
+document.write("<br> <br>This is boolean datatype use in const variable <br>"+ y);
+
+const Usama = ["mango", "bannan","orainge",];
+document.write("<br> <br>This is arry datatype use in const variable <br>"+ Usama);
+
+let object = [3454,1209,220,];
+document.write("<br> <br>This is object datatype use in let variable <br>"+ object);
+
+//All Arithmetic Operators 
+var a = 20;
+var b = 5;
+var c = 5;
+var sum =a+b+c;
+var sub =a-b-c;
+var divi =a/b/c;
+var mul =a*b*c;
+document.write("<br><br> subtraction=" +sum);
+document.write("<br> subtraction=" +sub);
+document.write("<br> division=" +divi);
+document.write("<br> Multiplication=" +mul);
+
+//Console With Aruthmetic Operators
+document.write("<br> <br>Console With Arithmetic")
+let u=10;
+let s=6;
+console.log("This Is console="+ u*s);
+
+//Console With Sstring
+document.write("<br> <br>Console With String & Number")
+console.log("USama Abbasi");
+console.log("console with numbmer="+ 9636 );
